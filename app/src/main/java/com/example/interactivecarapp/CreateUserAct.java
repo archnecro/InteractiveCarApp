e03af30082ec3288d6936b2525dc78d39e9905c5
kenziexbussell <mackenzie.bussell@faurecia.com>
2021-11-22 10:01:57 -0500

    added check for edit texts in CreateUserAct

--- app/src/main/java/com/example/interactivecarapp/CreateUserAct.java
+++ app/src/main/java/com/example/interactivecarapp/CreateUserAct.java
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class CreateUserAct extends AppCompatActivity {
     EditText etVin, etFirstName, etLastname, etAddress, etZip, etEmail, etPhone, etPass, etConfirm;
     Button btnSignUp;
     DBHelper dbHalp;
+    TextView tvError;
+    User newUser;
+
+    String vin, firstname, lastname, address, email, phone, pass, confirm;
+    int zip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -36,6 +43,9 @@ public class CreateUserAct extends AppCompatActivity {
         // db helper
         dbHalp = new DBHelper(this);
 
+        // text view
+        tvError = findViewById(R.id.tvError);
+
         // make sure the formatting for the phone number looks good
         etPhone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
 
@@ -44,8 +54,108 @@ public class CreateUserAct extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-
+               if (CheckTexts())
+               {
+                    GetTextValues();
+                    newUser = new User(email, pass, address, zip, phone, vin, firstname, lastname);
+               }
             }
         });
+    }
+
+    public boolean CheckTexts()
+    {
+        if (etVin.getText().toString().trim().length() == 0)
+        {
+            tvError.setText("*Please enter VIN number");
+            return false;
+        }
+        else if (etFirstName.getText().toString().trim().length() == 0)
+        {
+            tvError.setText("*All fields must be complete");
+            return false;
+        }
+        else if (etLastname.getText().toString().trim().length() == 0)
+        {
+            tvError.setText("*All fields must be complete");
+            return false;
+        }
+        else if (etAddress.getText().toString().trim().length() == 0)
+        {
+            tvError.setText("*All fields must be complete");
+            return false;
+        }
+        else if (etZip.getText().toString().trim().length() == 0)
+        {
+            tvError.setText("*All fields must be complete");
+            return false;
+        }
+        else if (etEmail.getText().toString().trim().length() == 0)
+        {
+            tvError.setText("*All fields must be complete");
+            return false;
+        }
+        else if (etPhone.getText().toString().trim().length() == 0)
+        {
+            tvError.setText("*All fields must be complete");
+            return false;
+        }
+        else if (etPass.getText().toString().trim().length() == 0)
+        {
+            tvError.setText("*All fields must be complete");
+            return false;
+        }
+        else if (etConfirm.getText().toString().trim().length() == 0)
+        {
+            tvError.setText("*Please confirm password");
+            return false;
+        }
+        else if (etVin.getText().toString().trim().length() < 17)
+        {
+            tvError.setText("*VIN not valid");
+            return false;
+        }
+        else if (etZip.getText().toString().trim().length() < 5)
+        {
+            tvError.setText("*Zip not valid");
+            return false;
+        }
+        else if (!Patterns.EMAIL_ADDRESS.matcher(etEmail.getText().toString().trim()).matches())
+        {
+            tvError.setText("*Email not valid");
+            return false;
+        }
+        else if (etPhone.getText().toString().trim().length() < 10)
+        {
+            tvError.setText("*Phone number not valid");
+            return false;
+        }
+        else if (!etPass.getText().toString().trim().equals(etConfirm.getText().toString().trim()))
+        {
+            tvError.setText("*Passwords did not match");
+            return false;
+        }
+        else if (dbHalp.CheckUserExists("SELECT * FROM user WHERE email=\"" + etEmail.getText().toString() + "\""))
+        {
+            tvError.setText("*User already exists");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public void GetTextValues()
+    {
+        vin = etVin.getText().toString().trim();
+        firstname = etFirstName.getText().toString().trim();
+        lastname = etLastname.getText().toString().trim();
+        address = etAddress.getText().toString().trim();
+        zip = Integer.parseInt(etZip.getText().toString());
+        email = etEmail.getText().toString().trim();
+        phone = etPhone.getText().toString().trim();
+        pass = etPass.getText().toString().trim();
+        confirm = etConfirm.getText().toString().trim();
     }
 }
--- app/src/main/java/com/example/interactivecarapp/OptionsMenuAct.java
+++ app/src/main/java/com/example/interactivecarapp/OptionsMenuAct.java
@@ -8,16 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class OptionsMenuAct extends AppCompatActivity {
     Intent LoginAct;
     Button btnKeyFob;
     TextView tvWelcome;
     TextView tvLog;
 
-    String passedName = "";
     String name = "";
-
-    boolean loggedin = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,6 +26,7 @@ public class OptionsMenuAct extends AppCompatActivity {
 
         // Intents
         LoginAct = new Intent(this, LoginAct.class);
+        Intent myIntent = getIntent();
 
         // Buttons
         btnKeyFob = findViewById(R.id.btnKeyFob);
@@ -35,17 +35,43 @@ public class OptionsMenuAct extends AppCompatActivity {
         tvWelcome = findViewById(R.id.tvWelcome);
         tvLog = findViewById(R.id.tvLog);
 
-        if (SaveUserInfo.getName(this).length() == 0)
+        // variable to determine whether to save username or not
+        boolean dontRemember = myIntent.getBooleanExtra("remember", false);
+
+        // passed name
+        String passedName = myIntent.getStringExtra("name");
+
+
+        if (dontRemember) // if user logs in but doesn't want to be remembered
         {
+            // capitalize first letter
+            passedName = passedName.substring(0, 1).toUpperCase(Locale.ROOT) + passedName.substring(1);
+            // set login/out text to logout
+            tvLog.setText(R.string.logout);
+            // clear saved username
+            SaveUserInfo.setName(getApplicationContext(), "");
+            // set top text to welcome user
+            tvWelcome.setText("Welcome Back " + passedName);
+        }
+        else if (SaveUserInfo.getName(this).length() == 0) // if user opens app and wasn't remembered
+        {
+            // finish will stop user from being able to go back to this page until logged in
+            finish();
+            // set login/out text to login
             tvLog.setText(R.string.login);
+            // start login activity
             OptionsMenuAct.this.startActivity(LoginAct);
         }
-        else
+        else // if user opens app and was remembered
         {
+            // set login/out text to logout
             tvLog.setText(R.string.logout);
+            // get saved user name
             name = SaveUserInfo.getName(getApplicationContext());
+            // capitalize first letter
+            name = name.substring(0, 1).toUpperCase(Locale.ROOT) + name.substring(1);
+            // set top text to welcome user
             tvWelcome.setText("Welcome Back " + name);
-            loggedin = true;
         }
 
         tvLog.setOnClickListener(new View.OnClickListener()
@@ -53,18 +79,9 @@ public class OptionsMenuAct extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-                if (loggedin)
-                {
-                    loggedin = false;
-                    SaveUserInfo.setName(getApplicationContext(), "");
-                    tvLog.setText(R.string.login);
-                    tvWelcome.setText("Welcome, Please Sign In To");
-                }
-                else
-                {
-                    OptionsMenuAct.this.startActivity(LoginAct);
-                }
-
+                SaveUserInfo.setName(getApplicationContext(), "");
+                finish();
+                OptionsMenuAct.this.startActivity(LoginAct);
             }
         });
 
